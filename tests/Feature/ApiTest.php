4ba186e7076bc4e8213c0f5093867794aa059c26
Brayan Duran <brayan.duran@leaguesofcode.com>
2022-09-17 03:49:16 +0200

    test added

--- tests/Feature/ApiTest.php
+++ tests/Feature/ApiTest.php
@@ -107,6 +107,10 @@ class ApiTest extends TestCase
 
         $response->assertStatus(200);
 
+        $response->assertJsonStructure([
+            'errors'
+        ]);
+
         $response->assertJsonPath('errors.0.message', 'Unauthenticated.');
     }
 
@@ -135,6 +139,10 @@ class ApiTest extends TestCase
             ', [], [], ['Authorization' => 'Bearer ' . $token]);
 
         $response->assertStatus(200);
+
+        $response->assertJsonStructure([
+            'data' => ['getStudents']
+        ]);
 
         $this->assertEquals(10, count($response['data']['getStudents']));
 
@@ -199,6 +207,10 @@ class ApiTest extends TestCase
 
         $response->assertStatus(200);
 
+        $response->assertJsonStructure([
+            'data' => ['getStudents']
+        ]);
+
         $this->assertEquals(10, count($response['data']['getStudents']));
 
         //Deleting students
@@ -214,6 +226,10 @@ class ApiTest extends TestCase
                 ', ['id' => $i], [], ['Authorization' => 'Bearer ' . $token]);
 
             $response->assertStatus(200);
+
+            $response->assertJsonStructure([
+                'data' => ['deleteStudent']
+            ]);
         }
 
         //Counting students
@@ -228,6 +244,193 @@ class ApiTest extends TestCase
 
         $response->assertStatus(200);
 
+        $response->assertJsonStructure([
+            'data' => ['getStudents']
+        ]);
+
         $this->assertEquals(0, count($response['data']['getStudents']));
+    }
+
+    public function test_add_student()
+    {
+        //Register
+
+        $response = $this->register();
+
+        //Get token
+
+        $token = $response['access_token'];
+
+        //Adding student
+
+        $response = $this->graphQL(/** @lang GraphQL */ '
+            mutation {
+                addStudent(firstname: "Brayan", lastname: "Duran",
+                email: "brayanduranmedina@gmail.com", birthdate: "2001-9-22",
+                address: "Carrer del Clot", score: 10) {
+                    id
+                }
+            }
+            ', [], [], ['Authorization' => 'Bearer ' . $token]);
+
+        $response->assertStatus(200);
+
+        $response->assertJsonStructure([
+            'data' => ['addStudent']
+        ]);
+
+        //Counting students
+
+        $response = $this->graphQL(/** @lang GraphQL */ '
+            {
+                getStudents {
+                    firstname
+                }
+            }
+            ', [], [], ['Authorization' => 'Bearer ' . $token]);
+
+        $response->assertStatus(200);
+
+        $response->assertJsonStructure([
+            'data' => ['getStudents']
+        ]);
+
+        $this->assertEquals(1, count($response['data']['getStudents']));
+    }
+
+    public function test_login_and_logout()
+    {
+        //Register format incorrect
+
+        $response = $this->post(
+            '/api/register',
+            [
+                'name' => 'BrayanD',
+                'email' => 'brayanduranmedina',
+                'password' => '12345678'
+            ]
+        );
+
+        $response->assertStatus(400);
+
+        //Register
+
+        $response = $this->register();
+
+        //Login format incorrect
+
+        $response = $this->post('/api/login', [
+            'email' => 'brayanduranmedina',
+            'password' => '12345678'
+        ]);
+
+        $response->assertStatus(400);
+
+        $this->app->get('auth')->forgetGuards();
+
+        //Login unauthenticated
+
+        $response = $this->post('/api/login', [
+            'email' => 'brayanduranmedina@gmail.com',
+            'password' => '123456789'
+        ]);
+
+        $response->assertStatus(401);
+
+        $this->app->get('auth')->forgetGuards();
+
+        //Login
+
+        $response = $this->post('/api/login', [
+            'email' => 'brayanduranmedina@gmail.com',
+            'password' => '12345678'
+        ]);
+
+        $response->assertStatus(200);
+
+        $this->app->get('auth')->forgetGuards();
+
+        //Get token
+
+        $token = $response['access_token'];
+
+        //Logout
+
+        $response = $this->post('/api/logoutall', [], ['Authorization' => 'Bearer ' . $token]);
+
+        $response->assertStatus(200);
+
+        $response->assertJsonPath('message', 'Successful logout');
+
+        $this->app->get('auth')->forgetGuards();
+    }
+
+    public function test_edit()
+    {
+        //Register
+
+        $response = $this->register();
+
+        //Get token
+
+        $token = $response['access_token'];
+
+        //Adding student
+
+        $response = $this->graphQL(/** @lang GraphQL */ '
+            mutation {
+                addStudent(firstname: "Brayan", lastname: "Duran",
+                email: "brayanduranmedina@gmail.com", birthdate: "2001-9-22",
+                address: "Carrer del Clot", score: 10) {
+                    firstname
+                }
+            }
+            ', [], [], ['Authorization' => 'Bearer ' . $token]);
+
+        $response->assertStatus(200);
+
+        $response->assertJsonStructure([
+            'data' => ['addStudent']
+        ]);
+
+        $response->assertJsonPath('data.addStudent.firstname', 'Brayan');
+
+        //Editing student fails incorrect email
+
+        $response = $this->graphQL(/** @lang GraphQL */ '
+            mutation {
+                addStudent(firstname: "Brayan", lastname: "Duran",
+                email: "brayanduranmedina", birthdate: "2001-9-22",
+                address: "Carrer del Clot", score: 10) {
+                    firstname
+                }
+            }
+            ', [], [], ['Authorization' => 'Bearer ' . $token]);
+
+        $response->assertStatus(200);
+
+        $response->assertJsonStructure([
+            'errors'
+        ]);
+
+        //Editing student
+
+        $response = $this->graphQL(/** @lang GraphQL */ '
+            mutation {
+                updateStudent(id: 1, firstname: "Daniel", lastname: "Duran",
+                email: "brayanduranmedina@gmail.com", birthdate: "2001-9-22",
+                address: "Carrer del Clot", score: 10) {
+                    firstname
+                }
+            }
+            ', [], [], ['Authorization' => 'Bearer ' . $token]);
+
+        $response->assertStatus(200);
+
+        $response->assertJsonStructure([
+            'data' => ['updateStudent']
+        ]);
+
+        $response->assertJsonPath('data.updateStudent.firstname', 'Daniel');
     }
 }
