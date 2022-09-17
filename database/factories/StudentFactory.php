8561a961d4d868c11bef18b2ed18f37d61283632
Brayan Duran <brayan.duran@leaguesofcode.com>
2022-09-17 03:10:52 +0200

    factory added

---
+++ database/factories/StudentFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Student>
+ */
+class StudentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'firstname' => fake()->firstName(),
+            'lastname' => fake()->lastName(),
+            'email' => fake()->unique()->safeEmail(),
+            'birthdate' => fake()->date(),
+            'address' => fake()->address(),
+            'score' => fake()->randomNumber(1),
+        ];
+    }
+}
